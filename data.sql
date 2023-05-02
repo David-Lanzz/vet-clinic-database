31ea846c19def33a86d74ef180b37d5d205de479
Olanrewaju Oduola <111298026+David-Lanzz@users.noreply.github.com>
2023-05-02 05:20:27 +0100

    Practice sql commands, create tables, insert data into tables and display specific tables

--- data.sql
+++ data.sql
@@ -1,5 +1,22 @@
-/* Populate database with sample data. */
+INSERT INTO animals
+VALUES (1,'Agumon','2020-03-02',0,false,10.23)
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals
+VALUES (2,'Gabumon','2018-11-15',2,true,8)
+
+
+UPDATE animals
+SET neutered = true 
+WHERE neutered = false
+
+INSERT INTO animals
+VALUES (3,'Pikachu','2021-01-07',1,false,15.04),(4,'Devimon','2017-05-12',5,true,11)
+
+UPDATE animals
+SET data_of_birth = '2020-02-03'
+WHERE data_of_birth = '2020-03-02'
+
+ALTER TABLE animals
+RENAME COLUMN data_of_birth TO date_of_birth
+
+SELECT * FROM animals ORDER BY id
--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+id INT NOT NULL,
+	name VARCHAR(50) NOT NULL,
+	data_of_birth DATE NOT NULL,
+	escape_attempts INT,
+	neutered BOOL,
+	weight_kg DECIMAL
 );
+
